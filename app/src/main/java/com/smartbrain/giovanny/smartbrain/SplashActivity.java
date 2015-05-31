cc40c88acbb0e4105723e3ff833542c958c6d736
giomm2 <giomm269@gmail.com>
2015-05-31 15:53:51 -0600

    Initial commit

---
+++ app/src/main/java/com/smartbrain/giovanny/smartbrain/SplashActivity.java
@@ -0,0 +1,103 @@
+package com.smartbrain.giovanny.smartbrain;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+
+
+public class SplashActivity extends ActionBarActivity {
+
+    private Thread mSplashThread;
+
+
+
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_splash);
+
+        final SplashActivity sPlashScreen = this;
+
+
+
+        mSplashThread =  new Thread(){
+
+            @Override
+
+            public void run(){
+
+                try {
+
+                    synchronized(this){
+
+
+
+                        wait(5000);
+
+                    }
+
+                }
+
+                catch(InterruptedException ex){
+
+                }
+
+
+
+                finish();
+
+
+
+                Intent intent = new Intent();
+
+                intent.setClass(sPlashScreen, MainActivity.class);
+
+                startActivity(intent);
+
+
+
+            }
+
+        };
+
+
+
+        mSplashThread.start();
+
+    }
+
+
+
+
+
+    @Override
+
+
+
+    public boolean onTouchEvent(MotionEvent evt)
+
+    {
+
+        if(evt.getAction() == MotionEvent.ACTION_DOWN)
+
+        {
+
+            synchronized(mSplashThread){
+
+                mSplashThread.notifyAll();
+
+            }
+
+        }
+
+        return true;
+
+    }
+
+}
