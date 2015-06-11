98725b84c088b69068dc0c66588e93d1f3a66118
maironcorrales <maironcorrales@gmail.com>
2015-06-11 13:18:04 -0600

    insercion de usuarios por medio de web service rest en la nube

--- app/src/main/java/com/smartbrain/giovanny/smartbrain/MainActivity.java
+++ app/src/main/java/com/smartbrain/giovanny/smartbrain/MainActivity.java
@@ -1,27 +1,29 @@
 package com.smartbrain.giovanny.smartbrain;
 
 import android.app.Activity;
-import android.support.v7.app.ActionBarActivity;
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
-import com.abcsoft.smarttbrain.services.ConnectionService;
-import com.abcsoft.smarttbrain.services.User;
-import com.abcsoft.smarttbrain.services.UserService;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
 
-import java.sql.SQLException;
-
+import org.json.JSONException;
+import org.json.JSONObject;
 
 
 public class MainActivity extends Activity {
 
     private Button playButton;
     private EditText username;
-    UserService service = new UserService();
+    ProgressDialog prgDialog;
+
 
 
 
@@ -33,24 +35,95 @@ public class MainActivity extends Activity {
 
         playButton= (Button)findViewById(R.id.btn_play);
         username = (EditText)findViewById(R.id.txt_name);
+        prgDialog = new ProgressDialog(this);
+        // Set Progress Dialog Text
+        prgDialog.setMessage("Please wait...");
+        // Set Cancelable as False
+        prgDialog.setCancelable(false);
 
         playButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String name = username.getText().toString();
+                String points = "0";
+                String idMail = "1";
+                RequestParams params = new RequestParams();
+                if (username.getText().toString().equals("")) {
+                    Toast.makeText(MainActivity.this, "Can´t leave name in blank", Toast.LENGTH_SHORT).show();
+                } else {
+                    params.put("name", name);
+                    params.put("points", points);
+                    params.put("idmail", idMail);
+                    // Invoke RESTful Web Service with Http parameters
+                    prgDialog.show();
+                    // Make RESTful webservice call using AsyncHttpClient object
+                    invokeWS(params);
 
-                User user = new User(username.getText().toString(),0,2);
-                try {
-                    service.insertUser(user);
-                } catch (ClassNotFoundException e) {
-                    e.printStackTrace();
-                } catch (SQLException e) {
-                    e.printStackTrace();
+
                 }
 
-
             }
+
         });
 
+
+
+    }
+
+    public void invokeWS(RequestParams params) {
+        // Show Progress Dialog
+        prgDialog.show();
+        // Make RESTful webservice call using AsyncHttpClient object
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get("http://162.219.247.87/register/doregister", params, new AsyncHttpResponseHandler() {
+            // When the response returned by REST has Http response code '200'
+            @Override
+            public void onSuccess(String response) {
+                // Hide Progress Dialog
+                prgDialog.hide();
+                try {
+                    // JSON Object
+                    JSONObject obj = new JSONObject(response);
+                    // When the JSON response has status boolean value assigned with true
+                    if (obj.getBoolean("status")) {
+                        // Set Default Values for Edit View controls
+
+                        // Display successfully registered message using Toast
+                        Toast.makeText(getApplicationContext(), "You are successfully registered!", Toast.LENGTH_LONG).show();
+                    }
+                    // Else display error message
+                    else {
+
+                        Toast.makeText(getApplicationContext(), obj.getString("error_msg"), Toast.LENGTH_LONG).show();
+                    }
+                } catch (JSONException e) {
+                    // TODO Auto-generated catch block
+                    Toast.makeText(getApplicationContext(), "Error Occured [Server's JSON response might be invalid]!", Toast.LENGTH_LONG).show();
+                    e.printStackTrace();
+
+                }
+            }
+
+            // When the response returned by REST has Http response code other than '200'
+            @Override
+            public void onFailure(int statusCode, Throwable error,
+                                  String content) {
+                // Hide Progress Dialog
+                prgDialog.hide();
+                // When Http response code is '404'
+                if (statusCode == 404) {
+                    Toast.makeText(getApplicationContext(), "Requested resource not found", Toast.LENGTH_LONG).show();
+                }
+                // When Http response code is '500'
+                else if (statusCode == 500) {
+                    Toast.makeText(getApplicationContext(), "Something went wrong at server end", Toast.LENGTH_LONG).show();
+                }
+                // When Http response code other than 404, 500
+                else {
+                    Toast.makeText(getApplicationContext(), "Unexpected Error occcured! [Most common Error: Device might not be connected to Internet or remote server is not up and running]", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 
     @Override
