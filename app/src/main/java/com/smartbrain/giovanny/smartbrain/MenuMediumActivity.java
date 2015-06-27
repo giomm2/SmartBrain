a7adcb4034da0dce16ca7c0c0b59ddb8ed36dfe3
cryven <john.vvargas@outlook.com>
2015-06-27 08:43:54 -0600

    En este commit se ven incluidos nada mas las pantallas de medium lvl en todas las dimensiones, las animaciones y sonidos estan pendientes

---
+++ app/src/main/java/com/smartbrain/giovanny/smartbrain/MenuMediumActivity.java
@@ -0,0 +1,18 @@
+package com.smartbrain.giovanny.smartbrain;
+
+import android.app.Activity;
+import android.widget.ImageView;
+
+/**
+ * Created by Cryven on 25/06/2015.
+ */
+public class MenuMediumActivity extends Activity {
+
+    private ImageView cloud;
+    private ImageView cloud1;
+    private ImageView cloud2;
+
+
+
+
+}
